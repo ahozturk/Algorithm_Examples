93be275a86dbeff4b395e91c7ca68178c44a1594
Hakan <ahmethakanozturk34@gmail.com>
2023-02-22 02:02:23 +0300

    Add Own Queue
    - Add Queue class

---
+++ Solutions/Solutions/Queue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class Queue<T>
+    {
+        private List<T> list;
+        public Queue()
+        {
+            list = new List<T>();
+        }
+        public void Enqueue(T item) => list.Add(item);
+        public T Dequeue()
+        {
+            if (list.Count != 0)
+            {
+                T temp = list[0];
+                list.RemoveAt(0);
+                return temp;
+            }
+            throw new IndexOutOfRangeException();
+        }
+        public bool IsEmpty() => list.Count == 0;
+        public int Size() => list.Count;
+    }
+}
